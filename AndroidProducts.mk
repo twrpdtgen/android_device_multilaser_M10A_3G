250ba0d92cf626f6f08d61f00b2fd23567c8492d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-18 06:59:47 +0100

    M10A_3G: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: multilaser Model: M10A_3G Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_M10A_3G.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_M10A_3G-user \
+    omni_M10A_3G-userdebug \
+    omni_M10A_3G-eng
---
+++ omni_M10A_3G.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from M10A_3G device
+$(call inherit-product, device/multilaser/M10A_3G/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := M10A_3G
+PRODUCT_NAME := omni_M10A_3G
+PRODUCT_BRAND := Multilaser
+PRODUCT_MODEL := M10A_3G
+PRODUCT_MANUFACTURER := multilaser
